be65ea3219fa68d1b55765e7f806c6f503ca292b
tecnomage <tecnocratay@gmail.com>
2022-07-28 23:27:47 -0300

    photo frame test created

--- src/app/shared/components/like-widget/like-widget.component.ts
+++ src/app/shared/components/like-widget/like-widget.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 import { UniqueIdService } from '../../services/unique-id/unique-id.service';
@@ -22,7 +23,6 @@ export class LikeWidgetComponent implements OnInit {
   }
 
   public like(): void {
-    console.log("like widget");
     this.liked.emit();
   }
 
--- src/app/shared/components/photo-frame/photo-frame.component.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,25 +1,41 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
   styleUrls: ['./photo-frame.component.scss'],
 })
-export class PhotoFrameComponent {
+export class PhotoFrameComponent  implements OnInit, OnDestroy {
   @Output() public liked: EventEmitter<void> = new EventEmitter();
   @Input() public description: string = '';
   @Input() public src: string = '';
   @Input() public likes: number = 0;
+  private debounceSubject: Subject<void> = new Subject();
+  private unsubscribe: Subject<void> = new Subject();
 
-  ngOnInit() {
-    console.log(this.description);
-    console.log(this.src);
-    console.log(this.likes);
+  ngOnInit():void {
+    console.log('photo frame init');
+    this.debounceSubject
+      .asObservable()
+      .pipe(debounceTime(500))
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(() => {
+        this.likes++;
+        this.liked.emit();
+      });
   }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();  
+  }
+
   constructor() {}
 
   public like(): void {
     console.log('like photo frame');
-    this.liked.emit();
+    this.debounceSubject.next();
   }
 }
